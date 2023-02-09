12a07f8d58346b1364d3c59221bce04f769c8715
Dulan Dias <dulan@echonlabs.com>
2023-02-09 12:35:11 +0000

    added find intersections function which uses the bisection method

---
+++ R/_find_intersections.R
@@ -0,0 +1,26 @@
+_find_intersections <- function(pu, mu){
+  intersections <- c()
+  for(i in 1:(length(mu)-1)){
+    for(j in (i+1):length(mu)){
+      x_l <- 0
+      x_u <- max(x)
+      f_l <- pu[i]*exp(-mu[i]*x_l) - pu[j]*exp(-mu[j]*x_l)
+      f_u <- pu[i]*exp(-mu[i]*x_u) - pu[j]*exp(-mu[j]*x_u)
+      while(x_u - x_l > 1e-10){
+        x_mid <- (x_u + x_l) / 2
+        f_mid <- pu[i]*exp(-mu[i]*x_mid) - pu[j]*exp(-mu[j]*x_mid)
+        if(f_mid == 0){
+          break
+        } else if(f_mid * f_l < 0){
+          x_u <- x_mid
+          f_u <- f_mid
+        } else {
+          x_l <- x_mid
+          f_l <- f_mid
+        }
+      }
+      intersections <- c(intersections, x_mid)
+    }
+  }
+  return(intersections)
+}
