15a87907ef5716c28d2cd6f05dd42c470b13546b
Dulan Dias <dulan@echonlabs.com>
2023-02-09 13:18:59 +0000

    added plot.pdf function

---
+++ R/plot.pdf.R
@@ -0,0 +1,7 @@
+plot.pdf <- function(x, emm) {
+  df <- data.frame(x = x, y = _pdf(x, emm))
+  ggplot(data=df, aes(x=x, y=y)) +
+    geom_line() + labs(y = "p(x)") +
+    ggtitle("Probability density function value, p(x)") +
+    theme(plot.title = element_text(size=8))
+}
