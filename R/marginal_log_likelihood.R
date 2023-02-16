c44c4131eb02c7dacd5a41aab89246389473fa54
Dulan Dias <dulan@echonlabs.com>
2023-02-16 11:51:12 +0000

    wip: refactoring mid-point

--- R/marginal_log_likelihood.R
+++ R/marginal_log_likelihood.R
@@ -1,5 +1,8 @@
 marginal_log_likelihood <- function(x, pi, mu) {
-  log_gamma <- log(matrix(pi, nrow = n, ncol = length(pi), byrow = TRUE)) - log(matrix(mu, nrow = n, ncol = length(mu), byrow = TRUE)) - matrix(x, ncol = 1) %*% (1 / matrix(mu, nrow = 1, ncol = length(mu)))
+
+  log_gamma <- t(replicate(length(x), pi)) - t(replicate(length(x), mu)) - matrix(x, ncol = 1) %*%  (1 / matrix(mu, nrow = 1))
+
   each_log_likelihood <- apply(log_gamma, 1, function(x) logsumexp(x))
+
   return(sum(each_log_likelihood))
 }
