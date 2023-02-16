1c7779211363e00fb80d6ecae7b482cbc790f82e
Dulan Dias <dulan@echonlabs.com>
2023-02-16 12:18:38 +0000

    added roxygen comments

--- R/aic.R
+++ R/aic.R
@@ -1,3 +1,21 @@
+#' Calculate Akaike Information Criterion (AIC)
+#'
+#' Calculates the Akaike Information Criterion (AIC) using the marginal log-likelihood and the number of parameters in the model.
+#'
+#' @param marginal_log_likelihood A scalar numeric value representing the marginal log-likelihood.
+#' @param k A scalar integer value representing the number of parameters in the model.
+#'
+#' @return A scalar numeric value representing the AIC.
+#'
+#' @examples
+#' marginal_log_likelihood <- -125.6
+#' k <- 3
+#' aic(marginal_log_likelihood, k)
+#'
+#' @export
+aic <- function(marginal_log_likelihood, k) {
+  return(-marginal_log_likelihood + 2 * k - 1)
+}
 aic <- function(marginal_log_likelihood, k) {
   return(-marginal_log_likelihood + 2 * k - 1)
 }
--- R/bic.R
+++ R/bic.R
@@ -1,3 +1,19 @@
+#' Calculates the Bayesian Information Criterion (BIC) for a model given its marginal log likelihood, number of parameters and sample size.
+#'
+#' The BIC is defined as -2 * marginal_log_likelihood + (2 * k - 1) * log(n) where k is the number of parameters and n is the sample size.
+#'
+#' @param marginal_log_likelihood The marginal log likelihood of the model.
+#' @param k The number of parameters in the model.
+#' @param n The sample size.
+#' @return The value of the BIC.
+#' @examples
+#' marginal_log_likelihood <- -10
+#' k <- 3
+#' n <- 100
+#' bic(marginal_log_likelihood, k, n)
+#' [1] 32.70554
+#'
+#' @export
 bic <- function(marginal_log_likelihood, k, n) {
   return(-marginal_log_likelihood + (2*k - 1) * log(n)/2)
 }
