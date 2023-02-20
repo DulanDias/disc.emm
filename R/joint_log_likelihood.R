a25b8db6f95bcfa7f1bf1471615cc5a44bd66e61
Dulan Dias <dulan@echonlabs.com>
2023-02-20 13:38:25 +0000

    added spaces

--- R/joint_log_likelihood.R
+++ R/joint_log_likelihood.R
@@ -1,9 +1,13 @@
 joint_log_likelihood <- function(n, n_each_exp, pi, mu) {
   temp <- 0
+
   for (j in 1:length(n_each_exp)) {
+
     if (n_each_exp[j] > 0) {
       temp <- temp + n_each_exp[j] * (log(pi[j]) - log(mu[j]))
     }
+
   }
+
   return(temp - n)
 }
